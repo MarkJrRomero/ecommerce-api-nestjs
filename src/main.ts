cce8426e17b7b49a08fbb11d2a2d91ca36a963e7
Mark Romero <mark.romero@energiasolarsa.com>
2025-07-18 11:55:49 -0500

    Ajustes para los CORS ⁉️

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Ecommerce API')
     .setDescription('Ecommerce API NestJS')
